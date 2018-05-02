ebdcbb327f2320c73ae5409e6e56d1092b1e4d90
msa <malginsa@gmail.com>
2018-05-02 08:56:17 +0300

    sorting integers

---
+++ src/main/java/l11/Generator.java
@@ -0,0 +1,29 @@
+package l11;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Generator {
+
+    public static String generateRandomString(int length, String alphabet)
+    {
+        char[] chars = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            int randomIndex = StdRandom.uniform(alphabet.length());
+            chars[i] = alphabet.charAt(randomIndex);
+        }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+
+        int count = Integer.parseInt(args[0]);
+        int lenght = Integer.parseInt(args[1]);
+        String alphabet = args[2];
+        for (int i=0; i < count; i++)
+        {
+            StdOut.println(generateRandomString(lenght, alphabet));
+        }
+    }
+}
---
+++ src/main/java/l11/TestSequentialSearch.java
@@ -0,0 +1,31 @@
+package l11;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class TestSequentialSearch {
+
+    public static int search (String key, String[] list)
+    {
+        for (int i=0; i<list.length; i++)
+            if (list[i].equals(key))
+                return i;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        String[] words = StdIn.readAllStrings();
+        int count = words.length;
+        double start = System.currentTimeMillis() / 1000.;
+        for (int i=0; i<10*count; i++)
+        {
+            String key = words[StdRandom.uniform(count)];
+            if (search(key, words) == -1)
+                StdOut.println(key);
+        }
+        double finish = System.currentTimeMillis() / 1000.;
+        StdOut.println(Math.round(finish-start) + " seconds");
+    }
+}
